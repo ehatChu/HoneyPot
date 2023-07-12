4031f1fbce90bebfd106a814730219ace061e5e7
DESKTOP-I8GD3QO\user2 <q>
2023-07-12 21:12:49 +0900

    design : 신고하기 모달바 생성

--- HoneyPot/src/main/java/com/hp/app/csc/controller/CSCController.java
+++ HoneyPot/src/main/java/com/hp/app/csc/controller/CSCController.java
@@ -8,34 +8,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("csc")
 public class CSCController {
 
+	// 회원
+	
 	// FAQ 조회(화면)
 	@GetMapping("faq")
 	public String getListFAQ() {
-		return "csc/faq";
+		return "csc/member/faq";
 	}
 	
 	// 문의하기(화면)
 	@GetMapping("inquiry")
 	public String inquiry() {
-		return "csc/inquiry";
+		return "csc/member/inquiry";
 	}
 	
 	// 신고하기(화면)
 	@GetMapping("report")
 	public String report() {
-		return "csc/report";
+		return "csc/member/report";
 	}
 	
 	// 문의목록 (화면)
 	@GetMapping("inquiry-list")
 	public String inquiryList() {
-		return "csc/inquiry-list";
+		return "csc/member/inquiry-list";
 	}
 	
 	// 신고목록 (화면)
 	@GetMapping("report-list")
 	public String reportList() {
-		return "csc/report-list";
+		return "csc/member/report-list";
 	}
+	
+	// 관리자
+	
+	// 1대1 상담내역(화면)
+	@GetMapping("admin/inquiry-list")
+	public String adminInquiryList() {
+		return "csc/admin/inquiry-list";
+	}
+	
+	// 신고내역(화면)
+	
+	// 제재내역(화면)
 	
 }
