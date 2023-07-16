938bc262dbd6865b5e12848e2d503f032de3d39e
Jyoong <>
2023-07-16 15:59:54 +0900

    feat : link to myReply & myLike / design :  add UI myReply & myLike, edit UI board & market

--- HoneyPot/src/main/java/com/hp/app/mypage/controller/MyPageController.java
+++ HoneyPot/src/main/java/com/hp/app/mypage/controller/MyPageController.java
@@ -12,10 +12,22 @@ public class MyPageController {
 		return "mypage/myInfo/calendar/view";
 	}
 	
-	
+	//내 게시글
 	@GetMapping("act/board")
 	public String myPost() {
 		return "mypage/act/board";
+	}
+	
+	//내 댓글
+	@GetMapping("act/reply")
+	public String myReply() {
+		return "mypage/act/reply";
+	}
+	
+	//좋아요한 글
+	@GetMapping("act/like")
+	public String myLike() {
+		return "mypage/act/like";
 	}
 	
 }
