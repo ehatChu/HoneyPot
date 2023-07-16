a15b9470de4196104ed9a11022e424a8976bc3df
DESKTOP-NOGAGFD\user <kms2786210@naver.com>
2023-07-17 02:35:20 +0900

    feat : 자동입력방지코드 기능 추가

--- HoneyPot/src/main/java/com/hp/app/data/controller/DataController.java
+++ HoneyPot/src/main/java/com/hp/app/data/controller/DataController.java
@@ -7,23 +7,21 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
 import javax.servlet.http.HttpServletRequest;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.expression.common.TemplateAwareExpressionParser;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hp.app.data.captcha.CaptchaUtil;
+import nl.captcha.Captcha;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 @RequestMapping("data")
 public class DataController {
 	@GetMapping("breakFast")
@@ -84,6 +82,46 @@ public class DataController {
 		conn.disconnect();
 
 		String result = sb.toString();
+		return result;
+	}
+
+	// 페이지 매핑
+	@GetMapping("/captcha.do")
+	public String Captcha() {
+		return "data/captcha";
+	}
+
+	// captcha 이미지 가져오는 메서드
+	@GetMapping("/captchaImg.do")
+	@ResponseBody
+	public void captchaImg(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		new CaptchaUtil().getImgCaptCha(req, res);
+	}
+
+	// 전달받은 문자열로 음성 가져오는 메서드
+	@GetMapping("/captchaAudio.do")
+	@ResponseBody
+	public void captchaAudio(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		Captcha captcha = (Captcha) req.getSession().getAttribute(Captcha.NAME);
+		String getAnswer = captcha.getAnswer();
+		new CaptchaUtil().getAudioCaptCha(req, res, getAnswer);
+	}
+
+	// 사용자가 입력한 보안문자 체크하는 메서드
+	@PostMapping("/chkAnswer.do")
+	@ResponseBody
+	public String chkAnswer(HttpServletRequest req, HttpServletResponse res) {
+		String result = "";
+		Captcha captcha = (Captcha) req.getSession().getAttribute(Captcha.NAME);
+		String ans = req.getParameter("answer");
+		if (ans != null && !"".equals(ans)) {
+			if (captcha.isCorrect(ans)) {
+				req.getSession().removeAttribute(Captcha.NAME);
+				result = "200";
+			} else {
+				result = "300";
+			}
+		}
 		return result;
 	}
 }
