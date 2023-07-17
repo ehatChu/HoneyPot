703f5fc443592ac30017fbeff885ebbde7ff2464
DESKTOP-J3DASVP\user2 <>
2023-07-17 20:24:03 +0900

    refactor : edit mapping

--- HoneyPot/src/main/java/com/hp/app/board/controller/BoardController.java
+++ HoneyPot/src/main/java/com/hp/app/board/controller/BoardController.java
@@ -5,47 +5,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("board")
+//@RequestMapping("board")
 public class BoardController {
 	
 	//게시글 작성
-	@GetMapping("write")
+	@GetMapping("board/write")
 	public String write() {
 		return "board/write";
 	}
 	
 	//게시글 목록 조회
-	@GetMapping("list")
+	@GetMapping("board/list")
 	public String list() {
 		return "board/list";
 	}
 	
 	//게시글 상세 조회
-	@GetMapping("detail")
+	@GetMapping("board/detail")
 	public String detail() {
 		return "board/detail";
 	}
 	
 	//장터 게시판
-	@GetMapping("market")
+	@GetMapping("board/market")
 	public String market() {
 		return "board/market";
 	}
 	
 	//내 게시글
-	@GetMapping("act/board")
+	@GetMapping("mypage/act/board")
 	public String myPost() {
 		return "mypage/act/board";
 	}
 	
 	//내 댓글
-	@GetMapping("act/reply")
+	@GetMapping("mypage/act/reply")
 	public String myReply() {
 		return "mypage/act/reply";
 	}
 	
 	//좋아요한 글
-	@GetMapping("act/like")
+	@GetMapping("mypage/act/like")
 	public String myLike() {
 		return "mypage/act/like";
 	}
