c98f2412c543fedfd686e940cd6f103e4740a4ef
yerinYoon <yer5_27@naver.com>
2023-07-18 20:36:58 +0900

    design : add Calendar Modal UI complete

--- HoneyPot/src/main/java/com/hp/app/calendar/controller/CalendarController.java
+++ HoneyPot/src/main/java/com/hp/app/calendar/controller/CalendarController.java
@@ -1,7 +1,9 @@
 package com.hp.app.calendar.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class CalendarController {
 	@GetMapping("info/calendar/mypage")
 	public String viewCalendar() {
