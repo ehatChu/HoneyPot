6d3111404144d2f23da020dc5be9d288b9a37f1c
DESKTOP-I8GD3QO\user2 <q>
2023-07-24 16:05:26 +0900

    fix : 썸머노트 버그 수정, url 수정

--- HoneyPot/src/main/java/com/hp/app/point/controller/PointController.java
+++ HoneyPot/src/main/java/com/hp/app/point/controller/PointController.java
@@ -1,7 +1,9 @@
 package com.hp.app.point.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PointController {
 
 	@GetMapping("mypage/act/point")
@@ -10,7 +12,7 @@ public class PointController {
 	}
 	
 	// 상벌점내역 (화면)
-	@GetMapping("point-list")
+	@GetMapping("admin/member/point-list")
 	public String pointList() {
 		return "admin/member/point-list";
 	}
