c963cf48d1bc88cf9cb5f4a84c21795285c33b7a
DESKTOP-NOGAGFD\user <kms2786210@naver.com>
2023-07-30 18:23:57 +0900

    feat : meal select start

--- HoneyPot/src/main/java/com/hp/app/meal/controller/MealController.java
+++ HoneyPot/src/main/java/com/hp/app/meal/controller/MealController.java
@@ -1,14 +1,37 @@
 package com.hp.app.meal.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hp.app.meal.service.MealService;
+import com.hp.app.meal.vo.MealVo;
+import com.hp.app.member.vo.MemberVo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("meal")
+@RequiredArgsConstructor
+@Slf4j
 public class MealController {
+	private final MealService ms;
+	
 	@GetMapping("mmeal")
-	public String mmain() {
+	public String mmain(HttpSession session, Model model) {
+		MemberVo loginMember = (MemberVo) session.getAttribute("loginMember");
+		if (loginMember == null) {
+			return "redirect:/main/mmain";
+		}
+		
+		List<MealVo> mealList = ms.getMealList();
+		model.addAttribute("mealList", mealList);
 		return "meal/mmeal";
 	}
 	
---
+++ HoneyPot/src/main/java/com/hp/app/meal/vo/MealVo.java
@@ -0,0 +1,14 @@
+package com.hp.app.meal.vo;
+
+import lombok.Data;
+
+@Data
+public class MealVo {
+	private String no;
+	private String dietNo;
+	private String breakfastDate;
+	private String menu;
+	private String nutrient;
+	private String img;
+	private String memberNo;
+}
